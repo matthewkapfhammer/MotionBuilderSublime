2f9b5ce66d247a968d07d2b831b21b5b47a824c8
Matthew Kapfhammer <github@lookslikematt.com>
2016-12-10 13:05:24 -0800

    #2 Updates manually testing steps.

--- tests/manual_testing.py
+++ tests/manual_testing.py
@@ -1,4 +1,15 @@
 """Misc lines of code to be used for manual testing of this plugin.
+
+MotionBuilderSublime's default shortcut to send code to 
+MotionBuilder is `ctrl+shift+enter`.
+
+To test sending individual lines: 
+    - Select lines you want to send> `ctrl+shift+enter`
+    - Check MotionBuilder to see if it executed as expected.
+
+To test sending entire file:
+    - Deselect all lines> `ctrl+shift+enter`
+    - Check MotionBuilder to see if it executed as expected.
 """
 # Test single line execution.
 FBModelCube('TestSingle')
